afb329b160dc66afff6ec4c4e25e42f19c59ab3a
aknight9255 <amanda.knight55@gmail.com>
2020-10-13 14:14:50 -0400

    After lunch Tuesday

---
+++ RestaurantRaterAPI/Models/Rating.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI.Models
+{
+    public class Rating
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [Range(0,10)]
+        public double FoodScore { get; set; }
+        [Required]
+        [Range(0,10)]
+        public double CleanlinessScore { get; set; }
+        [Required]
+        [Range(0, 10)]
+        public double EnviromentScore { get; set; }
+        public double AverageRating
+        {
+            get
+            {
+                return (FoodScore + EnviromentScore + CleanlinessScore) / 3;
+            }
+        }
+
+        [ForeignKey(nameof(Restaurant))]
+        public int RestaurantID { get; set; }
+        public virtual Restaurant Restaurant { get; set; }
+    }
+}
--- RestaurantRaterAPI/Models/Restaurant.cs
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -15,7 +15,56 @@ namespace RestaurantRaterAPI.Models
         [Required]
         public string Address { get; set; }
         [Required]
-        public double Rating { get; set; }
+        public double Rating
+        {
+            get
+            {
+                double totalAverageRating = 0;
+                foreach(var rating in Ratings)
+                {
+                    totalAverageRating += rating.AverageRating;
+                }
+                return (Ratings.Count > 0) ? totalAverageRating / Ratings.Count : 0;
+            }
+        }
+        //Average Food Rating 
+        public double AverageFoodScore
+        {
+            get
+            {
+                double totalFoodScore = 0;
+                foreach (var rating in Ratings)
+                {
+                    totalFoodScore += rating.FoodScore;
+                }
+                return (Ratings.Count > 0) ? totalFoodScore / Ratings.Count : 0;
+            }
+        }
+        //Average Environment Rating 
+        public double AverageEnvironmentScore
+        {
+            get
+            {
+                IEnumerable<double> scores = Ratings.Select(rating => rating.EnviromentScore);
+                double totalEnvironmentScore = scores.Sum();
+                return (Ratings.Count > 0) ? totalEnvironmentScore / Ratings.Count : 0;
+            }
+        }
+        //Average Cleanliness Rating 
+        public double AverageCleanlinessScore
+        {
+            get
+            {
+                //if(Ratings.Count > 0)
+                //{
+                //    return (Ratings.Select(r => r.CleanlinessScore).Average());
+                //}
+                //return 0;
+                return (Ratings.Count() > 0) ? Ratings.Select(r => r.CleanlinessScore).Average() : 0;
+
+            }
+        }
+        public virtual List<Rating> Ratings { get; set; } = new List<Rating>();
 
     }
 }
